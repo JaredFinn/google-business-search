69d6416fd35a98665f35c1c6a0e8408254349310
Full Name <simplyjustmy@gmail.com>
2024-03-03 13:40:35 -0500

    new services and server

--- src/app/google-maps.service.ts
+++ src/app/google-maps.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../environments/environment';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
@@ -8,23 +7,20 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class GoogleMapsService {
-  private apiKey = environment.googleMapsApiKey;
-  private placesUrl = 'https://maps.googleapis.com/maps/api/place/textsearch/json';
-  private detailsUrl = 'https://maps.googleapis.com/maps/api/place/details/json';
+  private functionUrl = '/.netlify/functions/googleMapsProxy'; // URL to the Netlify Function
 
   constructor(private http: HttpClient) { }
 
   searchPlaces(query: string): Observable<any[]> {
-    const url = `${this.placesUrl}?query=${encodeURIComponent(query)}&key=${this.apiKey}`;
+    const url = `${this.functionUrl}?path=textsearch&query=${encodeURIComponent(query)}`;
     return this.http.get<any>(url).pipe(
       map(response => response.results)
     );
   }
 
   getPlaceDetails(placeId: string): Observable<any> {
-    // Include 'formatted_phone_number' in the fields parameter
     const fields = 'name,website,formatted_phone_number';
-    const url = `${this.detailsUrl}?place_id=${placeId}&fields=${fields}&key=${this.apiKey}`;
+    const url = `${this.functionUrl}?path=details&place_id=${placeId}&fields=${fields}`;
     return this.http.get<any>(url).pipe(
       map(response => response.result)
     );
